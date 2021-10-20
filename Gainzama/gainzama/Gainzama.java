94da6a640d196870ed3a29e59aac4e9cf0b4575d
maiderHuarte <46961582+maiderHuarte@users.noreply.github.com>
2021-10-20 15:53:47 +0200

    Add files via upload

---
+++ Gainzama/gainzama/Gainzama.java
@@ -0,0 +1,34 @@
+
+package gainzama;
+
+/**
+ *
+ * Egileak Iñaki Goirizelaia eta Maider Huarte
+ */
+public class Gainzama {
+    
+    public int batu ( int a, int b ) { 
+        return ( a + b ); 
+    } 
+  
+
+    public int batu ( int a, int b, int k ) { 
+        return ( a + b + k ); 
+    } 
+  
+
+    public double batu ( double a, double b, double k ) { 
+        return ( a + b + k ); 
+    } 
+  
+    
+    public static void main( String parametroak [ ] ) {
+        
+        Gainzama gainzamatutakoBatu = new Gainzama( ); 
+        
+        System.out.println( gainzamatutakoBatu.batu( 123, 78 ) ); 
+        System.out.println( gainzamatutakoBatu.batu( 123, 78, 450 ) ); 
+        System.out.println( gainzamatutakoBatu.batu( 3.5, 37.89, 708.678 ) ); 
+    } 
+ 
+}
