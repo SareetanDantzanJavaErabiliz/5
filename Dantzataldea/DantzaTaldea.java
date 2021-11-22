532b06777202ebc8f9caa78ff122f16cd3b9863c
maiderHuarte <46961582+maiderHuarte@users.noreply.github.com>
2021-11-22 15:55:20 +0100

    Add files via upload

---
+++ Dantzataldea/DantzaTaldea.java
@@ -0,0 +1,26 @@
+/**
+ *
+ * Egileak: Iñaki Goirizelaia eta Maider Huarte
+ */
+
+public class DantzaTaldea {
+    
+    public static void main( String parametroak [ ] ) {
+        
+        Dantzari dantzariBat = new Dantzari( "Manex" );
+        Dantzari dantzariBi = new Dantzari( "Maialen" );
+          
+        // Metodoen bidez adina eta gustuko dantzak zehaztu       
+        dantzariBat.dantzariarenAdina( 18 );
+        dantzariBi.dantzariarenAdina( 22 );
+        
+        dantzariBat.gustukoDantza( "Bizkaiko Dantzari Dantza" );
+        dantzariBi.gustukoDantza( "Zuberoko Maskarada" );
+        
+        //Dantarien informazioa bistaratu
+        dantzariBat.dantzariaBistaratu( );
+        dantzariBi.dantzariaBistaratu( );
+                            
+    }
+    
+}
